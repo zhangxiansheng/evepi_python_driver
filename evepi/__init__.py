70131ae7d111b15f29e287104df7c5fe32cf8d96
zhangxiansheng <Karl1991223@126.com>
2016-01-21 14:19:57 +0800

    2

---
+++ evepi/__init__.py
@@ -0,0 +1,8 @@
+#Author:Zhang Yiwei
+#Email:
+#!/usr/bin/env python
+"""jerk.py is a simple library to make python more python"""
+
+__version__ = "0.2"
+__author__ = "Zhang Yiwei <Karl1991223@126.com>"
+__license__ = "public domain"
